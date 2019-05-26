829cdc751fe7af36c9d4f330e6918a5fa23544fd
Kash <castillo.sergio@gmail.com>
2019-05-26 17:38:51 +0200

    Modelo inicial

---
+++ src/Kash.Elector/ElectionResults.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kash.Elector
+{
+    public class ElectionResults
+    {
+        public virtual Election Election { get; protected set; }
+    }
+}
